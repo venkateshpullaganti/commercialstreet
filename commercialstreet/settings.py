a2d5c708b8a4e8b876af21a085c8b8e518762d17
venky <venkatesh@wealthy.in>
2024-06-04 01:27:44 +0530

    WIP Queryset

--- commercialstreet/settings.py
+++ commercialstreet/settings.py
@@ -34,10 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'debug_toolbar',
     'store',
     'tags',
     'likes'
--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from store.models import Collection, Customer, Order, OrderItem, Product
 # Create your views here.
 
-def calculate():
-    y = 4
-    x = 3
-    return x
 
 def say_hello(request):
-    x = calculate()
-    print(x)
-    
+    products = Product.objects.filter(collection__id=3)
+    customers = Customer.objects.filter(email__icontains='com')
+    collections = Collection.objects.filter(featured_product_id__isnull=True)
+    orders = Order.objects.filter(customer__id=1)
+    product_ids = list()
+    for product in products:
+        product_ids.append(product.id)
+    product_ids=tuple(product_ids)
+    print(len(product_ids))
+    products_orders = OrderItem.objects.filter(product__collection__id=3)
+    # print(len(collections))
+    print([products_order.unit_price for products_order in products_orders])
+
     # Using the Django template to return html with the context object
-    return render(request, "hello.html",{"name":"Jarvis"}) 
+    return render(request, "hello.html",{"name":"Jarvis", "products": list(products), "customers":list(customers)}) 
