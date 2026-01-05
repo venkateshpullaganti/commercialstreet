8b4a2ba47aeecfcafe2a518c2d4247b6f4d06eaf
venky <venkatesh@wealthy.in>
2026-01-06 00:58:50 +0530

    Dockerise the app

--- commercialstreet/settings/dev.py
+++ commercialstreet/settings/dev.py
@@ -15,26 +15,32 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "commercialstreet",
         'USER':'root',
-        'HOST':'localhost',
+        'HOST':'mysql',
         'PASSWORD':'admin123',
         'PORT': '3306',
     }   
 }
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK':lambda response:True
+}
